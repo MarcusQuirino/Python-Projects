e1992fb0c983a3e51782b59ed0043c8da93879cf
Marcus Filipe Quirino <marcus@Marcuss-MacBook-Air-2.local>
2022-06-03 11:40:21 -0300

    finished calculator

--- CliCalculator/calculator.py
+++ CliCalculator/calculator.py
@@ -12,9 +12,38 @@ def division(x, y):
 def muliplication(x, y):
     return x * y
 
+
 operations = {
 "+" : add, 
 "-": subtraction,
 "/": division,
 "*": muliplication
 }
+
+print(ascci)
+
+def calculator():
+    n1 = float(input("choose a number "))
+
+    for symbols in operations:
+        print(symbols)
+
+    again = True
+    while again:
+            function = input("choose one of the operation above ")
+
+            n2 = float(input("next number "))
+
+            calculation_function = operations[function]
+            answer = calculation_function(n1, n2)
+
+            print(f"{n1} {function} {n2} = {answer}\n")
+
+            loop = input(f"continue calculating with {answer}? [y] / [n] ") 
+            if loop == "y":
+                n1 = answer
+            else:
+                again = False
+                calculator()
+            
+calculator()
